28b3921f043e3ab400d46a58cfb044318839a7a7
kathy-ramirez <ramirezalanocakatherinaide@gmail.com>
2026-06-09 00:49:32 -0400

    implementacion de docker

--- backend-autopartes/src/app.module.ts
+++ backend-autopartes/src/app.module.ts
@@ -33,9 +33,9 @@ import { AuthModule } from './auth/auth.module';
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT') || 5432,
-        username: configService.get<string>('DB_USER'),
+        username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
+        database: configService.get<string>('DB_DATABASE'),
         autoLoadEntities: true, 
         synchronize: true, // ¡Recuerda apagarlo en producción!
 
